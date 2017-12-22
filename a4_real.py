25a020c05ab4a10661b8795126bc4145011c6cb9
XuJianzhi <30584453+XuJianzhi@users.noreply.github.com>
2017-12-22 14:39:54 +0800

    Update a4_real.py

--- a4_real.py
+++ a4_real.py
@@ -1,6 +1,7 @@
 
 
 
+
 #!/usr/bin/python
 # -*- coding: UTF-8 -*-
 import pandas as pd
@@ -32,6 +33,12 @@ test0_x=test0.copy()
 
 all_0=pd.concat([train0_x,test0_x])
 
+
+###
+#把nan换成-999
+all_0.fillna(-999,inplace=True)
+###
+
 #分定性和定量
 qualitative_0=pd.DataFrame()
 quantitative_0=pd.DataFrame()
@@ -50,17 +57,17 @@ for k in qualitative_1.columns:
 	le=sk.preprocessing.LabelEncoder()
 	qualitative_1[k][qualitative_1[k].notnull()]=le.fit_transform(qualitative_1[k][qualitative_1[k].notnull()])
 #定性补缺       
-imputer=Imputer(strategy='most_frequent')
-qualitative_1 = pd.DataFrame(imputer.fit_transform(qualitative_1),index=qualitative_1.index)
+#imputer=Imputer(strategy='most_frequent')
+#qualitative_1 = pd.DataFrame(imputer.fit_transform(qualitative_1),index=qualitative_1.index)
 #定性哑编码
 encoder=OneHotEncoder(sparse=False,dtype=np.int)
 qualitative_1 = pd.DataFrame(encoder.fit_transform(qualitative_1),index=qualitative_1.index)
 
 #定量补缺
-imputer=Imputer(strategy='mean')
-quantitative_1 = pd.DataFrame(imputer.fit_transform(quantitative_1),index=quantitative_1.index)
+#imputer=Imputer(strategy='mean')
+#quantitative_1 = pd.DataFrame(imputer.fit_transform(quantitative_1),index=quantitative_1.index)
 #数据变换（升维）
-pf=PolynomialFeatures(degree=3,interaction_only=True,include_bias=False)
+pf=PolynomialFeatures(degree=2,interaction_only=True,include_bias=False)
 quantitative_1 = pd.DataFrame(pf.fit_transform(quantitative_1),index=quantitative_1.index)
 
 #定性和定量合并
@@ -75,9 +82,8 @@ train1_y=train0_y.copy()
 #train1_y=train0_y.apply(float)
 '''
 #判别分析（降维）
-skb=SelectKBest(chi2,k=500)
+skb=SelectKBest(chi2,k=400)
 all_2=skb.fit_transform(train1_x,train1_y)
-test2_x=skb.transform(test1_x)
 '''
 ########################
 train_x, test_x, train_y, test_y_real = sk.model_selection.train_test_split(train1_x,train1_y,test_size=0.0)
@@ -96,21 +102,18 @@ def evalerror(preds, dtrain):
 
 params = {
             'objective': 'reg:gamma',
-            'eta': 0.01,
-            #'eval_metric': 'rmse',
-            #'eval_metric':evalerror,
-            #'eval_metric': 'mlogloss',
+            'eta': 0.002,
             'seed': 0,
             'missing': -999,
             #'num_class':num_class,
             'silent' : 1,
-            'gamma' : 0.01,
+            'gamma' : 0.02,
             'subsample' : 0.5,
-            'alpha' : 0.05,
-            'max_depth':7,
+            'alpha' : 0.045,
+            'max_depth':4,
             'min_child_weight':1
             }
-num_rounds=2000
+num_rounds=20000
 clf=xgb.train(params,dtrain,num_rounds,watchlist, feval=evalerror)
 
 '''
